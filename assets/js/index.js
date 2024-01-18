0d707db7ce7e60054dbf14c58744447eed0c4593
Josiah <josiahbergen1@gmail.com>
2024-01-18 13:50:52 -0800

    Initial commit!  🎉

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+
+
+// Fade in site content
+
+document.getElementById('main-content').style.display = 'block';
+document.getElementById('noscript-message').style.display = 'none';
+
+setTimeout(function() {
+	document.getElementById('section-header').style.opacity = 1;
+	document.getElementById('section-icons').style.opacity = 1;
+}, 10);
+
+setTimeout(function() {
+	document.getElementById('section-hoodie').style.opacity = 1;
+}, 30);
+
+setTimeout(function() {
+	document.getElementById('section-footer').style.opacity = 1;
+}, 40);
+
+// Borders when hovering on shopify link
+
+const imageOverlay = document.getElementById("hoodie-img-container");
+
+document.getElementById("hoodie-link").addEventListener('mouseenter', function() {
+	imageOverlay.style.setProperty("--border-opacity", 1);
+});
+
+document.getElementById("hoodie-link").addEventListener('mouseleave', function() {
+	imageOverlay.style.setProperty("--border-opacity", 0);
+});
+
+document.getElementById("hoodie-img").addEventListener('mouseenter', function() {
+	imageOverlay.style.setProperty("--border-opacity", 1);
+	imageOverlay.style.setProperty("--overlay-opacity", .4);
+});
+
+document.getElementById("hoodie-img").addEventListener('mouseleave', function() {
+	imageOverlay.style.setProperty("--border-opacity", 0);
+	imageOverlay.style.setProperty("--overlay-opacity", 0);
+});
+
