fcd710c6e5f610b71184766141fe5547438c1c12
Josiah <josiahbergen1@gmail.com>
2024-01-20 18:19:04 -0800

    updated some animations

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,25 +18,37 @@ setTimeout(function() {
 	document.getElementById('section-footer').style.opacity = 1;
 }, 40);
 
+
+// Resize body according to window dimensions
+function setHeight() {
+	if(document.body.scrollHeight < window.innerHeight)
+	document.getElementById("body").style.height = window.innerHeight + 'px';
+}
+
+setHeight();
+window.addEventListener('resize', setHeight);
+
 // Borders when hovering on shopify link
 
 const imageOverlay = document.getElementById("hoodie-img-container");
 
 document.getElementById("hoodie-link").addEventListener('mouseenter', function() {
-	imageOverlay.style.setProperty("--border-opacity", 1);
+	imageOverlay.style.setProperty("--rainbow-border-opacity", 1);
+	imageOverlay.style.setProperty("--gray-border-opacity", 0);
 });
 
 document.getElementById("hoodie-link").addEventListener('mouseleave', function() {
-	imageOverlay.style.setProperty("--border-opacity", 0);
+	imageOverlay.style.setProperty("--rainbow-border-opacity", 0);
+	imageOverlay.style.setProperty("--gray-border-opacity", 1);
 });
 
-document.getElementById("hoodie-img").addEventListener('mouseenter', function() {
-	imageOverlay.style.setProperty("--border-opacity", 1);
-	imageOverlay.style.setProperty("--overlay-opacity", .4);
-});
-
-document.getElementById("hoodie-img").addEventListener('mouseleave', function() {
-	imageOverlay.style.setProperty("--border-opacity", 0);
-	imageOverlay.style.setProperty("--overlay-opacity", 0);
-});
+//document.getElementById("hoodie-img").addEventListener('mouseenter', function() {
+//	imageOverlay.style.setProperty("--border-opacity", 1);
+//	imageOverlay.style.setProperty("--overlay-opacity", .4);
+//});
+//
+//document.getElementById("hoodie-img").addEventListener('mouseleave', function() {
+//	imageOverlay.style.setProperty("--border-opacity", 0);
+//	imageOverlay.style.setProperty("--overlay-opacity", 0);
+//});
 
